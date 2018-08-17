4490a1cf31f90846f84229bffc44800ff1e7e909
Amalie S <amaliesmidth@gmail.com>
2018-08-17 13:53:29 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,22 +6,22 @@ def assign_variable(value)
   name = value
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  return 'Nice'
 end
 
 def last_evaluated_value
-
+  return 'expert'
 end
 
-def pizza_party
-
+def pizza_party(thing)
+  return thing
 end
